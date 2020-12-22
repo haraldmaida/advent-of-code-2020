6fe88fb38794021e0b3fb572791c4bf775a37fd0
haraldmaida <harald.maida@innoave.com>
2020-12-22 07:45:58 +0100

    start with day 19 part 2 and postpone

--- src/day19/tests.rs
+++ src/day19/tests.rs
@@ -17,6 +17,56 @@ aaabbb
 aaaabbb
 "#;
 
+const EXAMPLE2: &str = r#"
+42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+"#;
+
 #[test]
 fn parse_monster_messages_in_example() {
     let monster_messages = parse_monster_messages(EXAMPLE);
@@ -108,6 +158,34 @@ fn count_matching_messages_in_puzzle_input() {
     let monster_messages = parse_monster_messages(INPUT);
 
     let count = count_matching_messages(&monster_messages);
+
+    assert_ne!(count, 14);
+    assert_eq!(count, 198);
+}
+
+#[test]
+fn count_matching_messages_in_example2() {
+    let monster_messages = parse_monster_messages(EXAMPLE2);
+
+    let count = count_matching_messages(&monster_messages);
+
+    assert_eq!(count, 3);
+}
+
+#[test]
+fn count_matching_messages_patched_rules_in_example2() {
+    let monster_messages = parse_monster_messages(EXAMPLE2);
+
+    let count = count_matching_messages_patched_rules(&monster_messages);
+
+    assert_eq!(count, 12);
+}
+
+#[test]
+fn count_matching_messages_patched_rules_in_puzzle_input() {
+    let monster_messages = parse_monster_messages(INPUT);
+
+    let count = count_matching_messages_patched_rules(&monster_messages);
 
     assert_ne!(count, 14);
     assert_eq!(count, 198);
